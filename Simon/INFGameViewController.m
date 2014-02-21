f3f83a84691c4dc8e6a417ea1750edff2f6262e7
influxd <jsacbibit@gmail.com>
2014-02-20 22:39:13 -0500

    Added segues and embedded main menu in navigation controller

--- Simon/INFGameViewController.m
+++ Simon/INFGameViewController.m
@@ -27,7 +27,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    [self.view.window setBackgroundColor:[UIColor blackColor]];
 }
 
 - (void)didReceiveMemoryWarning
